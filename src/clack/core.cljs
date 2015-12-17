50e02320971a853b2f1a102ba1cdff602439e629
Frazer Irving <frazer.irving@mailonline.com>
2015-12-17 16:36:30 +1100

    hey wow it works

--- src/clack/core.cljs
+++ src/clack/core.cljs
@@ -1,18 +1,26 @@
 (ns clack.core
   (:require [cljs.nodejs :as nodejs]
-            [cljs.pprint :refer [pprint]]))
+            [cljs.pprint :refer [pprint]]
+            [cljs.reader :refer [read-string]]))
 
 (nodejs/enable-util-print!)
 
 (defn slurp-stdin []
-  ;(js/process.stdin.setEncoding "utf8")
-  ;(js/process.stdout.setEncoding "utf8")
-  (let [input (atom "")]
+  (js/process.stdin.setEncoding "utf8")
+  (js/process.stdout.setEncoding "utf8")
+
+  (let [input (atom "")
+        query (when-let [args (not-empty (drop 2 (js->clj js/process.argv)))]
+                (read-string (first args)))]
     (js/process.stdin.on "readable" (fn []
                                       (when-let [chunk (js/process.stdin.read)]
                                         (swap! input #(str % chunk)))))
     (js/process.stdin.on "end" (fn []
-                                 (print @input)))))
+                                 (println :QQQ (type query) query)
+                                 (let [data (read-string @input)] 
+                                   (pprint (if query
+                                             (get-in data query)
+                                             data)))))))
 
 (defn -main [& args]
   (slurp-stdin))
