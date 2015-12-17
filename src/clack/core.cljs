ad59618d01210bb935554513283171470a777732
Frazer Irving <frazer.irving@mailonline.com>
2015-12-17 14:59:25 +1100

    IO working

--- src/clack/core.cljs
+++ src/clack/core.cljs
@@ -4,8 +4,18 @@
 
 (nodejs/enable-util-print!)
 
+(defn slurp-stdin []
+  ;(js/process.stdin.setEncoding "utf8")
+  ;(js/process.stdout.setEncoding "utf8")
+  (let [input (atom "")]
+    (js/process.stdin.on "readable" (fn []
+                                      (when-let [chunk (js/process.stdin.read)]
+                                        (swap! input #(str % chunk)))))
+    (js/process.stdin.on "end" (fn []
+                                 (print @input)))))
+
 (defn -main [& args]
-  (println "Hello world!"))
+  (slurp-stdin))
 
 (set! *main-cli-fn* -main)
 
